a5fa66592752e3fe57354f786da87fe2a60d6b27
udita <uditagowdety@gmail.com>
2024-11-05 13:02:47 +0530

    starting reaction diffusion!

--- fractal trees/sketch.js
+++ fractal trees/sketch.js
@@ -1,36 +1,31 @@
-var angle=0;
-var slider;
+let slider;
 
 function setup() {
     createCanvas(400, 400);
-    background(220);
-    slider=createSlider(0,TWO_PI,PI/4,0.01);
+    slider = createSlider(0, TWO_PI, PI / 4, 0.01);
+    slider.position(width*1.45, height/2);
+    slider.parent(document.querySelector('.intro'));
 }
 
 function draw() {
     background(51);
-    // var len =100;
-    angle=slider.value();
+    let angle = slider.value();
     stroke(255);
-    translate(200,height)
-    // line(200,height, 200,height-len);
-    branch(100)
+    translate(200, height);
+    branch(100, angle);
 }
 
-function branch(len){
-    line(0,0,0,-len);
-    translate(0,-len);
-    
-    if (len>4){
+function branch(len, angle) {
+    line(0, 0, 0, -len);
+    translate(0, -len);
+    if (len > 4) {
         push();
         rotate(angle);
-        branch(len*0.67)
+        branch(len * 0.67, angle);
         pop();
         push();
         rotate(-angle);
-        branch(len*0.67);
+        branch(len * 0.67, angle);
         pop();
     }
-
-    // line(0,0,0,-len*0.67);
 }
